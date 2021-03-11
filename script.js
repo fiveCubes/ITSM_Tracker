718e7a8e8bc7a092fa67bc7fa18687d702c9f354
Vishnu Vijayakumar <54991428+fiveCubes@users.noreply.github.com>
2021-03-11 20:40:07 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+var countDownDate = new Date("March 17, 2021 15:00:00").getTime();
+  
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+  
+      // Get todays date and time
+      var now = new Date().getTime();
+      
+      // Find the distance between now an the count down date
+      var distance = Math.abs(countDownDate - now);
+  
+      console.log(distance)
+      
+      // Time calculations for days, hours, minutes and seconds
+      let Hours = Math.floor(distance / ( 1000 * 60 * 60 ));
+      let hours_left = distance % (1000 *60 * 60)
+      let min = Math.floor(hours_left /(1000*60))
+      let min_left = hours_left % (1000*60)
+      let sec = Math.floor(min_left /1000)
+  
+      console.log(Hours)
+      console.log(min)
+      console.log(sec)
+      
+      // Output the result in an element with id="demo"
+      document.getElementById("timer").innerHTML = Hours +' :' + min +' :' +sec
+      
+      // If the count down is over, write some text 
+      if (distance < 0) {
+          clearInterval(x);
+          document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+  }, 1000);
+  
